4406ee45b2a3d6e02ae4995342224174d3e3b178
carolynembulwa <carolynembulwa@gmail.com>
2020-11-23 12:55:14 +0300

    Floyd’s Cycle-finding Algorithm

--- src/com/nodeball/LinkedList.java
+++ src/com/nodeball/LinkedList.java
@@ -157,9 +157,13 @@ public class LinkedList {
 
     }
 
-    public void getKthFromTheEnd(int kth){
+    public int getKthFromTheEnd(int kth){
         //find the Kth Node from the end of a linkedlist in one pass
         int k = kth;
+        // if size is provide you can check if it is greater than size
+        if(k>size){
+            throw new IllegalArgumentException();
+        }
 
         // [10 -> 20 -> 30 -> 40 -> 50 ]
         //               *          *
@@ -171,35 +175,72 @@ public class LinkedList {
 
         //check if it is empty
         if (isEmpty()) {
-            return;
+            throw new IllegalStateException();
         }
 
-        Node current  = first;
 
 
+        Node a = first;
+        Node b = first;
+
+        for( int i = 0; i <k -1; i++){
+            b = b.next;
+            //if we dont know the size we  can check if b is null
+            if(b == null){
+                throw new IllegalArgumentException();
+            }
+        }
+        while(b !=last){
+            a = a.next;
+            b = b.next;
+        }
+        return a.value;
 
     }
+    public void middleInonePass(){
+        //Floyd’s Cycle-finding Algorithm
+        if (isEmpty()) {
+            throw new IllegalStateException();
+        }
+        Node a = first;
+        Node b = first;
+
+        while (b !=last && b.next !=last){
+            b = b.next.next;
+            a = a.next;
+        }
+         if(b == last){
+             System.out.println(a.value);
+         }else{
+             System.out.println(a.value + "," + a.next.value);
+         }
+    }
+
+    //   Find the middle of a linked list in one pass.
+    //    // If the list has an even number of nodes, there would be two
+    //    // middle nodes. (Note: Assume that you don’t know the size of the
+    //    // list ahead of time
 
     public void reverse(){
-        //Given a linked list, write a function that prints
+        // 1. Given a linked list, write a function that prints
         // the nodes of the list in reverse order.
 
 
     }
 
-   // 1.Question:​ Given a linked list, determine whether
+   // 2.Question:​ Given a linked list, determine whether
     // it contains a cycle.
 
-    //2. Dedup Linked List
+    //3. Dedup Linked List
     //Question:​ Given an unsorted linked list, write a function to remove all the duplicates. eg.
     //dedup(​1​ -> ​2​ -> ​3​ -> ​2​ -> ​1​) = ​1​ -> ​2​ -> ​3
 
-    //3 Given a linked list, write a function
+    //4. Given a linked list, write a function
     // to split the list into two equal halves. eg.
     //divide(​1​ -> ​2​ -> ​3​ -> ​4​) = ​1​ -> ​2​, ​3​ -> ​4
     //divide(​1​ -> ​2​ -> ​3​ -> ​4​ -> ​5​) = ​1​ -> ​2​ -> ​3​, ​4​ -> ​5
 
-    //4.Nth to the Last Element
+    //5.Nth to the Last Element
     //Question:​ Given a linked list, and an input n, write a function
     // that returns the nth-to-last element of the linked list.
     //e.g
@@ -210,7 +251,7 @@ public class LinkedList {
     // nthToLast(​list​, ​4​) = 1
     // nthToLast(​list​, ​5​) = null
 
-    //5. 39. Random Linked List
+    //6. Random Linked List
     //Question:​ Given a linked list where each node has
     // two pointers, one to the next node
     // and one to a random node in the list, clone the linked list.
@@ -220,5 +261,8 @@ public class LinkedList {
     //  |    |    |    |
     //  v    v    v    v
     //  3​    ​1​    ​3​    ​2
+
+
+
 
 }
